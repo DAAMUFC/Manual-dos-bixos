6b9eb5306b9a3e6671fe2eea387b5e81ad1fefc8
Paulo Douglas <douglassilva2@yahoo.com.br>
2024-01-14 17:35:17 -0300

    Ajuste nos links do menu

--- elements/menu/menu.js
+++ elements/menu/menu.js
@@ -1,8 +1,8 @@
 function selected(event) {
 	event.preventDefault();
-	page = document.querySelector(".menu a").getAttribute("href")
+	page = event.target.attributes[1].value;
 	setTimeout(() => {
-						window.location.href = `${document.querySelector("a").getAttribute("href")}?course=${course}`;
+						window.location.href = `${page}?course=${urlParams.get('course')}`;
 	                }, 200);
 	soundSelect = new Audio('./sounds/8-Bit.m4a');
 	soundSelect.play();
